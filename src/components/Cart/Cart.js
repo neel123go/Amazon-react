d7c1243f6fbf127c6279c93521b9523fe071ee67
neel123go <ayounpaul1029@gmail.com>
2022-03-23 20:23:54 +0600

    final commit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,10 +1,15 @@
+import { faArrowRight, faTrashCan } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { deleteShoppingCart } from '../../utilities/fakedb';
 import './Cart.css';
 
 const Cart = ({ cart }) => {
     let total = 0;
     let shipping = 0;
+    let quantity = 0;
     for (const product of cart) {
+        quantity = quantity + product.quantity;
         total = total + product.price;
         shipping = shipping + product.shipping;
     }
@@ -13,11 +18,13 @@ const Cart = ({ cart }) => {
     return (
         <div className='cart'>
             <h5>Order Summary</h5>
-            <p>Selected Items: {cart.length}</p>
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${total}</p>
             <p>Total Shipping Charge: ${shipping}</p>
             <p>Tax: ${tax}</p>
             <h5>Grand Total: ${grandTotal}</h5>
+            <button className='btn btn-danger w-100 mt-4' onClick={deleteShoppingCart}>Clear Cart <FontAwesomeIcon icon={faTrashCan}></FontAwesomeIcon></button>
+            <button className='btn w-100 review-order-btn'>Review Order <FontAwesomeIcon icon={faArrowRight}></FontAwesomeIcon></button>
         </div>
     );
 };
